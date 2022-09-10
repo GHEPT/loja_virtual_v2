4cc8b550533787aa3c9bd966cc3563549c47fcb1
teodo <teodo@192.168.1.65>
2022-09-10 17:08:37 -0300

    Commit inicial projeto mentoria java

---
+++ src/main/java/loja_virtual_v2/LojaVirtualV2Application.java
@@ -0,0 +1,13 @@
+package loja_virtual_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/loja_virtual_v2/LojaVirtualV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package loja_virtual_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
