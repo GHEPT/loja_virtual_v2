f9c2d25442ee41dd956c57f2cdbadc69ab8e5829
teodo <teodo@192.168.1.65>
2022-09-10 17:31:42 -0300

    Configurado sppicaltionProperties e Renomeado pacote

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualV2Application.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualV2Application.java
@@ -1,4 +1,4 @@
-package loja_virtual_v2;
+package jdev.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
